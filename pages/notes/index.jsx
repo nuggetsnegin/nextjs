a433dec5af1892ebe3878d8813ef15d89bd3efd2
Negin Sauermann <nuggetnegin@gmail.com>
2020-07-29 12:08:24 -0400

    multiple notes, note about as

--- pages/notes/index.jsx
+++ pages/notes/index.jsx
@@ -1,14 +1,24 @@
+// pages/notes/index.jsx
 import React from 'react'
 import Link from 'next/link'
 
+export default () => {
+  const notes = new Array(15).fill(1).map((e, i) => ({id: i, title: `Note: ${i}`}))
 
-const Page = () => (
-  <div>
-    <h1>Notes Index Path</h1>
-    <Link href="/notes/[id]" as={`/notes/1`}>
-      <a>Note 1</a>
-    </Link>
-  </div>
-)
+  return (
+    <div>
+      <h1>Notes</h1>
 
-export default Page
+      {notes.map(note => (
+        <div>
+          {/* note: "as" is used when you want to hide the actual ID and show other content instead */}
+          <Link key={note.id} href="/notes/[id]" as={`/notes/${note.id}`}>
+            <a>
+              <strong>{note.title}</strong>
+            </a>
+          </Link>
+        </div>
+      ))}
+    </div>
+  )
+}
