70ec335ed181ae7d36fe3ef691189967e961d0eb
Negin Sauermann <nuggetnegin@gmail.com>
2020-07-29 15:56:16 -0400

    fixed notes import

--- pages/notes/index.jsx
+++ pages/notes/index.jsx
@@ -24,7 +24,6 @@ export default ({notes}) => {
   )
 }
 
-
 export async function getServerSideProps() {
   const res = await fetch(`http://localhost:3000/api/note`)
   const {data} = await res.json()
