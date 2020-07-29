350a4adafa58614ac1d3b8c4e324c0dd5a617e4c
Negin Sauermann <nuggetnegin@gmail.com>
2020-07-29 14:59:19 -0400

    api handlers done

---
+++ pages/api/note/index.js
@@ -0,0 +1,20 @@
+import nc from 'next-connect'
+import notes from '../../../src/data/data'
+
+const handler = nc()
+    .post((req, res) => {
+        const note = {
+            //whatever is on request.body
+            ...req.body,
+            id: Date.now()
+        }
+        //push to our db of notes
+        notes.push(note);
+        res.json({data: note})
+    })
+    //get all the notes
+    .get((req, res) => {
+        res.json({data: notes})
+    })
+ 
+export default handler
---
+++ src/components/data/data.js
@@ -0,0 +1,7 @@
+// src/data/data.js
+//in memory array of notes nothing flashy
+const notes = []
+
+
+//must do module.export or else it wont persist
+module.exports = notes
